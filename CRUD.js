a2833c95deb0d2a2c7a2f8e5d966b44f5a59edd4
codetecch <33946703+codetecch@users.noreply.github.com>
2018-03-21 12:16:08 +1100

    Update CRUD.js

--- CRUD.js
+++ CRUD.js
@@ -1,70 +1,47 @@
+//to define http libararies , to make http requests.
+var http = require("http")
 
 //To define postgre libraries
 var pg = require("pg")
 
-//to define http libararies , to make http requests.
-var http = require("http")
-
-    var port = 5433;
+var p = 5400;
 
 //Local data to perform actions.
 var data = [{"name":"aman", "Sex": "Male"},{"mohit":"Manoj", "Sex":"Male"},{"monika":"Sai","Sex":"female"}];
 	
 
-	var writeResults = function(req, res){
-		console.log("Write Results");
-		res.writeHead(200, {'Content-Type': 'text/plain',
-							'Access-Control-Allow-Origin' : '*'});
-		res.write(data);
-		res.end();
-	};
-	
-
 //The below instructions is the example to get connect with the database.
 	var getRecords = function(req, res, callback){
 		console.log("Get Records");
-        //Local url for postgresql domain.
+                //Local url for postgresql domain.
 		var conString = "pg://postgres:3267635@localhost:5432/TestDB";
-var client = new pg.Client(conString);
-client.connect();
+                var client = new pg.Client(conString);
+                client.connect();
 	
 
-//query the database to get data.
-		var query = client.query('select id,fname,lname from "Student"  order by id');
-query.on("row", function(row, result)
-{
-    result.addRow(row);
-});
+//query the database to insert data.
+        var query = client.query('insert id,fname,lname from "Student"  order by id');
+          query.on("row", function(row, result)
+              {
+                 result.addRow(row);
+              });
 
-        //to represnt the results extract from database.
-		query.on("end", function(result)
-{
+//to represnt the results extract from database.
+query.on("end", function(result)
+{                 
     client.end();
     data = JSON.stringify(result.rows, null, "    ");
     //console.log(data);
     callback(req, res);
 });
 	};
-	
 
-
-	var updateRecords = function(req, res, callback){
-		console.log("update Records");
-		//var conString = "pg://postgres:3267635@localhost:5432/TestDB";
-		//var client = new pg.Client(conString);
-		client.connect();
-		console.log(req.body);
-		//var query =  
-	}	
-	
 
 //Below instructions helps to call the function  execute above
 	http.createServer(function(req, res) {
 		if(req.method == 'GET') {
 		console.log("Get Request in main");
 		getRecords(req, res, writeResults);
-		//list_data(req,res);
-		//setInterval(list_records,3000);
 		}
 		if(req.method == 'POST'){
 			updateRecords(req, res, writeResults)
